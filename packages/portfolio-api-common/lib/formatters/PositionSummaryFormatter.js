157d77875f009a009984ff97a9debe69828058d8
Steve Widerberg <steve.widerberg@barchart.com>
2018-03-14 12:31:17 -0500

    Add position summary formatter

---
+++ packages/portfolio-api-common/lib/formatters/PositionSummaryFormatter.js
@@ -0,0 +1,71 @@
+const array = require('@barchart/common-js/lang/array')
+	assert = require('@barchart/common-js/lang/assert'),
+	Decimal = require('@barchart/common-js/lang/Decimal'),
+	formatter = require('@barchart/common-js/lang/formatter');
+
+module.exports = (() => {
+
+	/**
+	 * Formats Position Summary records into groups based on instrument type
+	 *
+	 */
+	class PositionSummaryFormatter{
+
+		/**
+		 * The formatter
+		 *
+		 * @param {Array} summaries
+		 * @returns {Object}
+		 */
+		static format(summaries) {
+			assert.argumentIsRequired(summaries, 'summaries', Object);
+
+			const summaryGroups = array.groupBy(summaries, summary => summary.instrument.type.code);
+
+			const total = {
+				start: Decimal.ZERO,
+				change: Decimal.ZERO,
+				end: Decimal.ZERO
+			};
+
+			const formattedSummaryGroups = Object.keys(summaryGroups).map((group) => {
+				const positions = summaryGroups[group];
+
+				let start = Decimal.ZERO;
+				let end = Decimal.ZERO;
+
+				positions.map((position) => {
+					start = start.add(position.start.value);
+					end = end.add(position.end.value);
+				});
+
+				const change = end.subtract(start);
+
+				total.start = total.start.add(start);
+				total.change = total.change.add(change);
+				total.end = total.end.add(end);
+
+				return summaryGroups[group] = {
+					name: group,
+					start: formatNumber(start.toFloat()),
+					end: formatNumber(end.toFloat()),
+					change: formatNumber(change.toFloat())
+				};
+			});
+
+			Object.keys(total).map(key => total[key] = formatNumber(total[key].toFloat()));
+
+			return {
+				groups: formattedSummaryGroups,
+				total
+			};
+		}
+	}
+
+	const formatNumber = (float) => {
+		return formatter.numberToString(float, 2, ',');
+	};
+
+	return PositionSummaryFormatter;
+
+})();
