d6ba1bc6a2c6822e92976485dc2e51200b8a1efa
Bryan Ingle <bryan.ingle@barchart.com>
2018-04-16 17:05:50 -0500

    Fix

--- packages/portfolio-api-common/lib/api/failures/PortfolioFailureType.js
+++ packages/portfolio-api-common/lib/api/failures/PortfolioFailureType.js
@@ -71,7 +71,7 @@ module.exports = (() => {
 		}
 	}
 
-	const positionCreateFailedNoPortfolio = new FailureType('POSITION_CREATE_FAILED_OUT_OF_SEQUENCE', 'Unable to create transaction. The referenced portfolio does not exist. Has it been deleted?');
+	const positionCreateFailedNoPortfolio = new FailureType('POSITION_CREATE_FAILED_NO_PORTFOLIO', 'Unable to create transaction. The referenced portfolio does not exist. Has it been deleted?');
 
 	const transactionCreateFailedOutOfSequence = new FailureType('TRANSACTION_CREATE_FAILED_OUT_OF_SEQUENCE', 'Unable to create transaction, because the transaction date is out-of-sequence. In other words, it would occur before an existing transaction. Please confirm your intent to re-write transaction history (which could take some time and alter the historical results for this position).');
 	const transactionCreateFailedDirectionSwitch = new FailureType('TRANSACTION_CREATE_FAILED_DIRECTION_SWITCH', 'Unable to create transaction, because the position direction would be switched (from long to short or vice versa). Please close the position (to a zero balance) first, then enter a second transaction.');
