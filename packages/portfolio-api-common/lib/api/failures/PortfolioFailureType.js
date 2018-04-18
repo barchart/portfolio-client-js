c58f93a098668c26cb972a7efe3b11f0ffc5e996
Bryan Ingle <bryan.ingle@barchart.com>
2018-04-18 17:23:16 -0500

    Transaction Validation (WIP)

--- packages/portfolio-api-common/lib/api/failures/PortfolioFailureType.js
+++ packages/portfolio-api-common/lib/api/failures/PortfolioFailureType.js
@@ -35,15 +35,21 @@ module.exports = (() => {
 		}
 
 		/**
-		 * The transaction type is not valid, given the current position size
-		 * (e.g. sell is invalid for a short position, sell short must be used).
-		 *
 		 * @public
 		 * @static
 		 * @returns {FailureType}
 		 */
-		static get TRANSACTION_CREATE_FAILED_TYPE_INVALID_FOR_POSITION() {
-			return transactionCreateFailedTypeInvalidForPosition;
+		static get TRANSACTION_CREATE_FAILED_TYPE_INVALID_FOR_INSTRUMENT() {
+			return transactionCreateFailedTypeInvalidForInstrument;
+		}
+
+		/**
+		 * @public
+		 * @static
+		 * @returns {FailureType}
+		 */
+		static get TRANSACTION_CREATE_FAILED_TYPE_INVALID_FOR_DIRECTION() {
+			return transactionCreateFailedTypeInvalidForDirection;
 		}
 
 		/**
@@ -54,12 +60,8 @@ module.exports = (() => {
 		 * @static
 		 * @returns {FailureType}
 		 */
-		static get TRANSACTION_CREATE_FAILED_DIRECTION_SWITCH() {
-			return transactionCreateFailedDirectionSwitch;
-		}
-
-		static get TRANSACTION_CREATE_REWRITE_UNSUPPORTED() {
-			return transactionCreateRewriteUnsupported;
+		static get TRANSACTION_CREATE_FAILED_INVALID_DIRECTION_SWITCH() {
+			return transactionCreateFailedInvalidDirectionSwitch;
 		}
 
 		/**
@@ -86,9 +88,9 @@ module.exports = (() => {
 	const positionCreateFailedNoPortfolio = new FailureType('POSITION_CREATE_FAILED_NO_PORTFOLIO', 'Unable to create transaction. The referenced portfolio does not exist. Has it been deleted?');
 
 	const transactionCreateFailedOutOfSequence = new FailureType('TRANSACTION_CREATE_FAILED_OUT_OF_SEQUENCE', 'Unable to create transaction, because the transaction date is out-of-sequence. In other words, it would occur before an existing transaction. Please confirm your intent to re-write transaction history (which could take some time and alter the historical results for this position).');
-	const transactionCreateFailedTypeInvalidForPosition = new FailureType('TRANSACTION_CREATE_FAILED_INVALID_FOR_POSITION', 'A {L|type.description} are not allowed. At the time of the transaction, your position is {L|direction} (i.e. {L|sign}).');
-	const transactionCreateFailedDirectionSwitch = new FailureType('TRANSACTION_CREATE_FAILED_DIRECTION_SWITCH', 'Unable to create transaction, because the position direction would be switched (from long to short or vice versa). Please close the position (to a zero balance), then enter a second transaction.');
-	const transactionCreateRewriteUnsupported = new FailureType('TRANSACTION_CREATE_REWRITE_UNSUPPORTED', 'Unable to re-write transaction history. This operation is not currently supported (but will be implemented soon).');
+	const transactionCreateFailedTypeInvalidForInstrument = new FailureType('TRANSACTION_CREATE_FAILED_TYPE_INVALID_FOR_INSTRUMENT', '{L|transactionType.description} are not allowed for {L|instrumentType.description}');
+	const transactionCreateFailedTypeInvalidForDirection = new FailureType('TRANSACTION_CREATE_FAILED_TYPE_INVALID_FOR_DIRECTION', 'A {L|type.description} are not allowed. At the time of the transaction, your position is {L|direction} (i.e. {L|sign}).');
+	const transactionCreateFailedInvalidDirectionSwitch = new FailureType('TRANSACTION_CREATE_FAILED_INVALID_DIRECTION_SWITCH', 'Unable to create transaction, because the position direction would be switched (from long to short or vice versa). Please close the position (to a zero balance), then enter a second transaction.');
 
 	const transactionDeleteFailedOutOfSequence = new FailureType('TRANSACTION_DELETE_FAILED_OUT_OF_SEQUENCE', 'Deleting any transaction, except for the most recent, will cause transaction history to be re-written. Please confirm your intent to re-write transaction history (which could take some time and alter the historical results for this position).');
 	const transactionDeleteUnsupported = new FailureType('TRANSACTION_DELETE_UNSUPPORTED', 'Unable to delete transaction. This operation is not currently supported (but will be implemented soon).');
