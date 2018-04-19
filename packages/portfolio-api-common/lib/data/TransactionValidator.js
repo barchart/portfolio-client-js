e899323cb4328b58cf02f10381bed5568dab6662
Bryan Ingle <bryan.ingle@barchart.com>
2018-04-19 13:38:49 -0500

    Tweak

--- packages/portfolio-api-common/lib/data/TransactionValidator.js
+++ packages/portfolio-api-common/lib/data/TransactionValidator.js
@@ -132,7 +132,7 @@ module.exports = (() => {
 	associateTypes(InstrumentType.OTHER, TransactionType.SELL, true, [ PositionDirection.LONG ]);
 	associateTypes(InstrumentType.OTHER, TransactionType.INCOME, true, [ PositionDirection.LONG ]);
 	associateTypes(InstrumentType.OTHER, TransactionType.FEE, true, [ PositionDirection.LONG ]);
-	associateTypes(InstrumentType.OTHER, TransactionType.VALUATION, true);
+	associateTypes(InstrumentType.OTHER, TransactionType.VALUATION, true, [ PositionDirection.LONG ]);
 
 	associateTypes(InstrumentType.CASH, TransactionType.DEPOSIT, true);
 	associateTypes(InstrumentType.CASH, TransactionType.WITHDRAWAL, true);
