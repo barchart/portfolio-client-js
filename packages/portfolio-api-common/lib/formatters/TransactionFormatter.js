211239bbda9d157d8b4000a850702144c0c486af
Bryan Ingle <bryan.ingle@barchart.com>
2018-05-30 16:17:42 -0500

    Make sorting consistent for descending order (date and sequence)

--- packages/portfolio-api-common/lib/formatters/TransactionFormatter.js
+++ packages/portfolio-api-common/lib/formatters/TransactionFormatter.js
@@ -273,7 +273,7 @@ module.exports = (() => {
 	}).thenBy((a, b) => {
 		return comparators.compareStrings(a.instrument.id, b.instrument.id);
 	}).thenBy((a, b) => {
-		return comparators.compareNumbers(a.sequence, b.sequence);
+		return comparators.compareNumbers(b.sequence, a.sequence);
 	}).toComparator();
 
 	return TransactionFormatter;
