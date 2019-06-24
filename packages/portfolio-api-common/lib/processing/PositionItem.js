0c8b2d8fead1eac73d70dd188ecf0d42d44d1864
Bryan Ingle <bryan.ingle@barchart.com>
2019-06-24 11:05:26 -0500

    Oops, correct previous commit.

--- packages/portfolio-api-common/lib/processing/PositionItem.js
+++ packages/portfolio-api-common/lib/processing/PositionItem.js
@@ -571,12 +571,12 @@ module.exports = (() => {
 				data.periodUnrealizedChange = periodUnrealizedChange;
 			} else {
 				data.unrealizedChange = Decimal.ZERO;
-				data.periodUnrealized = Decimal.ZERO;
+				data.periodUnrealizedChange = Decimal.ZERO;
 				data.periodGainChange = Decimal.ZERO;
 			}
 		} else {
 			data.unrealizedChange = Decimal.ZERO;
-			data.periodUnrealized = Decimal.ZERO;
+			data.periodUnrealizedChange = Decimal.ZERO;
 			data.periodGainChange = Decimal.ZERO;
 		}
 	}
