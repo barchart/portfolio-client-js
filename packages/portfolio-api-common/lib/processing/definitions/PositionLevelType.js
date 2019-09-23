be4fa723135e3be26e009e9d4d8efdad9111537a
Bryan Ingle <bryan.ingle@barchart.com>
2019-09-23 09:38:02 -0500

    JSDoc

--- packages/portfolio-api-common/lib/processing/definitions/PositionLevelType.js
+++ packages/portfolio-api-common/lib/processing/definitions/PositionLevelType.js
@@ -3,19 +3,49 @@ const Enum = require('@barchart/common-js/lang/Enum');
 module.exports = (() => {
 	'use strict';
 
+	/**
+	 * Describes the contents of a grouping level within a tree of positions.
+	 * This is an attribute of a {@link PositionLevelDefinition}.
+	 *
+	 * @public
+	 * @extends {Enum}
+	 */
 	class PositionLevelType extends Enum {
 		constructor(code) {
 			super(code, code);
 		}
 
+		/**
+		 * A level of grouping that represents an entire portfolio's contents.
+		 *
+		 * @public
+		 * @static
+		 * @returns {PositionLevelType}
+		 */
 		static get PORTFOLIO() {
 			return portfolio;
 		}
 
+		/**
+		 * A level of grouping that represents a single positions (i.e. guaranteed to
+		 * be a leaf node in a grouping tree).
+		 *
+		 * @public
+		 * @static
+		 * @return {PositionLevelType}
+		 */
 		static get POSITION() {
 			return position;
 		}
 
+		/**
+		 * A level of grouping that is neither a portfolio or a position. This could be an
+		 * intermediate level of grouping (e.g. an asset class within a portfolio).
+		 *
+		 * @public
+		 * @static
+		 * @return {PositionLevelType}
+		 */
 		static get OTHER() {
 			return other;
 		}
