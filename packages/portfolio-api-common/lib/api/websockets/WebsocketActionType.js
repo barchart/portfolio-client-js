4489a7008bb5b084139213c6ae1cb27d281a2a75
Dzmitry Afanasenka <dzmitry.afanasenka@itechart-group.com>
2019-11-13 14:49:02 +0300

    Create WebsocketActionType class

---
+++ packages/portfolio-api-common/lib/api/websockets/WebsocketActionType.js
@@ -0,0 +1,123 @@
+const assert = require('@barchart/common-js/lang/assert'),
+  Enum = require('@barchart/common-js/lang/Enum');
+
+module.exports = (() => {
+  'use strict';
+
+  /**
+   * Types of websocket response actions.
+   *
+   * @public
+   * @extends {Enum}
+   * @param {String} code
+   * @param {String} description
+   * @param {String} action
+   */
+  class WebsocketActionType extends Enum {
+    constructor(code, description, action) {
+      super(code, description);
+
+      assert.argumentIsRequired(action, 'action', String);
+
+      this._action = action;
+    }
+
+    /**
+     * An action of websocket response
+     *
+     * @public
+     * @returns {String}
+     */
+    get action() {
+      return this._action;
+    }
+
+    /**
+     * Action type for creating new portfolio.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get PORTFOLIO_CREATE() {
+      return portfolioCreate;
+    }
+
+    /**
+     * Action type for deleting portfolio.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get PORTFOLIO_DELETE() {
+      return portfolioDelete;
+    }
+
+    /**
+     * Action type for updating new portfolio.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get PORTFOLIO_UPDATE() {
+      return portfolioUpdate;
+    }
+
+    /**
+     * Action type for creating new transaction.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get TRANSACTION_CREATE() {
+      return transactionCreate;
+    }
+
+    /**
+     * Action type for deleting transaction.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get TRANSACTION_DELETE() {
+      return transactionDelete;
+    }
+
+    /**
+     * Action type for editing transaction.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get TRANSACTION_EDIT() {
+      return transactionEdit;
+    }
+
+    /**
+     * Action type for batching transaction.
+     *
+     * @public
+     * @static
+     * @returns {WebsocketActionType}
+     */
+    static get TRANSACTION_BATCH() {
+      return transactionBatch;
+    }
+  }
+
+  const portfolioCreate = new WebsocketActionType('PORTFOLIO_CREATE', 'Create portfolio action', 'portfolio/create');
+  const portfolioDelete = new WebsocketActionType('PORTFOLIO_DELETE', 'Delete portfolio action', 'portfolio/delete');
+  const portfolioUpdate = new WebsocketActionType('PORTFOLIO_UPDATE', 'Update portfolio action', 'portfolio/update');
+
+  const transactionCreate = new WebsocketActionType('TRANSACTION_CREATE', 'Create transaction action', 'transaction/create');
+  const transactionDelete = new WebsocketActionType('TRANSACTION_DELETE', 'Delete transaction action', 'transaction/delete');
+  const transactionEdit = new WebsocketActionType('TRANSACTION_EDIT', 'Edit transaction action', 'transaction/edit');
+  const transactionBatch = new WebsocketActionType('TRANSACTION_BATCH', 'Batch transaction action', 'transaction/batch');
+
+  return WebsocketActionType;
+})();
