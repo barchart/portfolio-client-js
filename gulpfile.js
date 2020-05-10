8b92a09b774a225042b1181f9d17af2d25513ef7
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-05-10 09:09:07 -0500

    Remove gulp jsdoc commands, will use @barchart/barchart-documentation instead

--- gulpfile.js
+++ gulpfile.js
@@ -42,15 +42,6 @@ gulp.task('embed-version', () => {
 		.pipe(gulp.dest('./lib/'));
 });
 
-gulp.task('document', (cb) => {
-	exec('jsdoc . -c jsdoc.json -r -d docs', (error, stdout, stderr) => {
-		console.log(stdout);
-		console.log(stderr);
-
-		cb();
-	});
-});
-
 gulp.task('commit-changes', () => {
 	return gulp.src([ './', './test/', './package.json', './lib/index.js', './example/example.js', './test/SpecRunner.js' ])
 		.pipe(git.add())
@@ -108,7 +99,6 @@ gulp.task('execute-tests', gulp.series(
 gulp.task('release', gulp.series(
 	'ensure-clean-working-directory',
 	'execute-tests',
-	'document',
 	'bump-version',
 	'embed-version',
 	'build-example-bundle',
