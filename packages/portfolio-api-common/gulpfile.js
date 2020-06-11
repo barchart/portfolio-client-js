02885509f91f3fd1010acd17cc6625136e1e7510
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-06-11 12:19:17 -0500

    Use interactive prompt for version selection in gulp "release" script

--- packages/portfolio-api-common/gulpfile.js
+++ packages/portfolio-api-common/gulpfile.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 
 const browserify = require('browserify'),
 	buffer = require('vinyl-buffer'),
-	bump = require('gulp-bump'),
+	exec = require('child_process').exec,
 	git = require('gulp-git'),
 	gitStatus = require('git-get-status'),
 	glob = require('glob'),
 	jasmine = require('gulp-jasmine'),
 	jshint = require('gulp-jshint'),
+	prompt = require('gulp-prompt'),
 	source = require('vinyl-source-stream');
 
 function getVersionFromPackage() {
@@ -26,10 +27,31 @@ gulp.task('ensure-clean-working-directory', (cb) => {
 	});
 });
 
-gulp.task('bump-version', () => {
-	return gulp.src([ './package.json' ])
-		.pipe(bump({ type: 'patch' }))
-		.pipe(gulp.dest('./'));
+gulp.task('bump-choice', (cb) => {
+	const processor = prompt.prompt({
+		type: 'list',
+		name: 'bump',
+		message: 'What type of bump would you like to do?',
+		choices: ['patch', 'minor', 'major'],
+	}, (res) => {
+		global.bump = res.bump;
+
+		return cb();
+	});
+
+	return gulp.src(['./package.json']).pipe(processor);
+});
+
+gulp.task('bump-version', (cb) => {
+	exec(`npm version ${global.bump || 'patch'} --no-git-tag-version`, {
+		cwd: './'
+	}, (error) => {
+		if (error) {
+			cb(error);
+		}
+
+		cb();
+	});
 });
 
 gulp.task('commit-changes', () => {
@@ -81,6 +103,7 @@ gulp.task('execute-tests', gulp.series(
 gulp.task('release', gulp.series(
 	'ensure-clean-working-directory',
 	'execute-tests',
+	'bump-choice',
 	'bump-version',
 	'commit-changes',
 	'push-changes',
