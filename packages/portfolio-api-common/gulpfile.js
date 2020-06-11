f1b66b3781c5b232737694762e5eca4374d238ab
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-06-11 12:16:14 -0500

    JSHint uses ES2018 now

--- packages/portfolio-api-common/gulpfile.js
+++ packages/portfolio-api-common/gulpfile.js
@@ -89,7 +89,7 @@ gulp.task('release', gulp.series(
 
 gulp.task('lint', () => {
 	return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./test/SpecRunner.js' ])
-		.pipe(jshint({'esversion': 6}))
+		.pipe(jshint({'esversion': 9}))
 		.pipe(jshint.reporter('default'))
 		.pipe(jshint.reporter('fail'));
 });
