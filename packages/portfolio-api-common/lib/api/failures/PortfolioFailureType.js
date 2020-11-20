1994d4ebf6bae50ec4b5e3faf1e5b58a0b7ccff9
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-11-20 12:40:52 -0700

    Tweak error message

--- packages/portfolio-api-common/lib/api/failures/PortfolioFailureType.js
+++ packages/portfolio-api-common/lib/api/failures/PortfolioFailureType.js
@@ -389,7 +389,7 @@ module.exports = (() => {
 	const transactionEditFailedInvalidDate = new FailureType('TRANSACTION_EDIT_FAILED_INVALID_DATE', 'Unable to edit transaction with given date.');
 	const transactionEditFailedNoTransaction = new FailureType('TRANSACTION_EDIT_FAILED_NO_TRANSACTION', 'Unable to edit transaction. The referenced transaction does not exist.', false);
 	const transactionEditFailedTypeReserved = new FailureType('TRANSACTION_EDIT_FAILED_TYPE_RESERVED', 'Unable to edit {U|type.description} transaction, this type of transaction is managed by the system.');
-	const transactionEditFailedTypeChanged = new FailureType('TRANSACTION_EDIT_FAILED_TYPE_CHANGED', 'Changing a transaction type is forbidden. You must delete the existing transaction then recreate it.');
+	const transactionEditFailedTypeChanged = new FailureType('TRANSACTION_EDIT_FAILED_TYPE_CHANGED', 'Changing a transaction type is forbidden. You must delete the existing transaction and then create a new transaction.');
 
 	return PortfolioFailureType;
 })();
