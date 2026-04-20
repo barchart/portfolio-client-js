4b8bff5bcc9c413730e0bce66c853fb485eac733
Bryan Ingle <bryaningle@gmail.com>
2026-04-19 20:02:01 -0600

    feat: add `snaptrade.link.mode` to portfolio schemas (and re-order attribtues)

--- packages/portfolio-api-common/lib/serialization/PortfolioSchema.js
+++ packages/portfolio-api-common/lib/serialization/PortfolioSchema.js
@@ -5,6 +5,7 @@ const Currency = require('@barchart/common-js/lang/Currency'),
 	Timezones = require('@barchart/common-js/lang/Timezones');
 
 const SnapTradeLinkStatus = require('./../data/SnapTradeLinkStatus'),
+	SnapTradeLinkMode = require('./../data/SnapTradeLinkMode'),
 	ValuationType = require('./../data/ValuationType');
 
 module.exports = (() => {
@@ -96,96 +97,123 @@ module.exports = (() => {
 	const complete = new PortfolioSchema(SchemaBuilder.withName('complete')
 		.withField('user', DataType.STRING)
 		.withField('portfolio', DataType.STRING)
+
 		.withField('name', DataType.STRING)
 		.withField('timezone', DataType.forEnum(Timezones, 'Timezone'))
+		.withField('miscellany', DataType.AD_HOC, true)
+		.withField('email', DataType.AD_HOC, true)
+
 		.withField('dates.create', DataType.DAY)
 		.withField('dates.access', DataType.TIMESTAMP, true)
+
 		.withField('defaults.cash', DataType.BOOLEAN, true)
 		.withField('defaults.currency', DataType.forEnum(Currency, 'Currency'))
 		.withField('defaults.reinvest', DataType.BOOLEAN, true)
 		.withField('defaults.valuation', DataType.forEnum(ValuationType, 'ValuationType'))
-		.withField('snaptrade.connection', DataType.STRING, true)
-		.withField('snaptrade.account', DataType.STRING, true)
-		.withField('snaptrade.broken', DataType.BOOLEAN, true)
-		.withField('snaptrade.timestamp', DataType.TIMESTAMP, true)
-		.withField('snaptrade.link.status', DataType.forEnum(SnapTradeLinkStatus, 'SnapTradeLinkStatus'), true)
-		.withField('snaptrade.link.progress', DataType.NUMBER, true)
-		.withField('snaptrade.link.timestamp', DataType.TIMESTAMP, true)
-		.withField('snaptrade.brokerage.display', DataType.STRING, true)
-		.withField('snaptrade.brokerage.logo', DataType.STRING, true)
+
 		.withField('legacy.system', DataType.STRING, true)
 		.withField('legacy.user', DataType.STRING, true)
 		.withField('legacy.portfolio', DataType.STRING, true)
 		.withField('legacy.warnings', DataType.NUMBER, true)
 		.withField('legacy.drops', DataType.NUMBER, true)
-		.withField('miscellany', DataType.AD_HOC, true)
-		.withField('email', DataType.AD_HOC, true)
-		.withField('system.calculate.processors', DataType.NUMBER, true)
+
+		.withField('snaptrade.connection', DataType.STRING, true)
+		.withField('snaptrade.account', DataType.STRING, true)
+		.withField('snaptrade.broken', DataType.BOOLEAN, true)
+		.withField('snaptrade.timestamp', DataType.TIMESTAMP, true)
+
+		.withField('snaptrade.brokerage.display', DataType.STRING, true)
+		.withField('snaptrade.brokerage.logo', DataType.STRING, true)
+
+		.withField('snaptrade.link,mode', DataType.forEnum(SnapTradeLinkMode, 'SnapTradeLinkMode'), true)
+		.withField('snaptrade.link.status', DataType.forEnum(SnapTradeLinkStatus, 'SnapTradeLinkStatus'), true)
+		.withField('snaptrade.link.progress', DataType.NUMBER, true)
+		.withField('snaptrade.link.timestamp', DataType.TIMESTAMP, true)
+
 		.withField('system.sequence', DataType.NUMBER)
 		.withField('system.version', DataType.STRING)
 		.withField('system.timestamp', DataType.TIMESTAMP)
+		.withField('system.calculate.processors', DataType.NUMBER, true)
+
 		.schema
 	);
 
 	const client = new PortfolioSchema(SchemaBuilder.withName('client')
 		.withField('user', DataType.STRING)
 		.withField('portfolio', DataType.STRING)
+
 		.withField('name', DataType.STRING)
 		.withField('timezone', DataType.forEnum(Timezones, 'Timezone'))
+		.withField('miscellany', DataType.AD_HOC, true)
+		.withField('email', DataType.AD_HOC, true)
+
 		.withField('dates.create', DataType.DAY)
 		.withField('dates.access', DataType.TIMESTAMP, true)
+
 		.withField('defaults.cash', DataType.BOOLEAN, true)
 		.withField('defaults.currency', DataType.forEnum(Currency, 'Currency'))
 		.withField('defaults.reinvest', DataType.BOOLEAN, true)
 		.withField('defaults.valuation', DataType.forEnum(ValuationType, 'ValuationType'))
-		.withField('snaptrade.connection', DataType.STRING, true)
-		.withField('snaptrade.account', DataType.STRING, true)
-		.withField('snaptrade.broken', DataType.BOOLEAN, true)
-		.withField('snaptrade.timestamp', DataType.TIMESTAMP, true)
-		.withField('snaptrade.link.status', DataType.forEnum(SnapTradeLinkStatus, 'SnapTradeLinkStatus'), true)
-		.withField('snaptrade.link.progress', DataType.NUMBER, true)
-		.withField('snaptrade.link.timestamp', DataType.TIMESTAMP, true)
-		.withField('snaptrade.brokerage.display', DataType.STRING, true)
-		.withField('snaptrade.brokerage.logo', DataType.STRING, true)
+
 		.withField('legacy.system', DataType.STRING, true)
 		.withField('legacy.user', DataType.STRING, true)
 		.withField('legacy.portfolio', DataType.STRING, true)
 		.withField('legacy.warnings', DataType.NUMBER, true)
 		.withField('legacy.drops', DataType.NUMBER, true)
-		.withField('miscellany', DataType.AD_HOC, true)
-		.withField('email', DataType.AD_HOC, true)
+
+		.withField('snaptrade.connection', DataType.STRING, true)
+		.withField('snaptrade.account', DataType.STRING, true)
+		.withField('snaptrade.broken', DataType.BOOLEAN, true)
+		.withField('snaptrade.timestamp', DataType.TIMESTAMP, true)
+
+		.withField('snaptrade.brokerage.display', DataType.STRING, true)
+		.withField('snaptrade.brokerage.logo', DataType.STRING, true)
+
+		.withField('snaptrade.link,mode', DataType.forEnum(SnapTradeLinkMode, 'SnapTradeLinkMode'), true)
+		.withField('snaptrade.link.status', DataType.forEnum(SnapTradeLinkStatus, 'SnapTradeLinkStatus'), true)
+		.withField('snaptrade.link.progress', DataType.NUMBER, true)
+		.withField('snaptrade.link.timestamp', DataType.TIMESTAMP, true)
+
 		.withField('system.calculate.processors', DataType.NUMBER, true)
+
 		.schema
 	);
 
 	const name = new PortfolioSchema(SchemaBuilder.withName('name')
 		.withField('user', DataType.STRING)
 		.withField('portfolio', DataType.STRING)
+
 		.withField('name', DataType.STRING)
+
 		.schema
 	);
 
 	const create = new PortfolioSchema(SchemaBuilder.withName('create')
 		.withField('name', DataType.STRING)
 		.withField('timezone', DataType.forEnum(Timezones, 'Timezone'))
+		.withField('miscellany', DataType.AD_HOC, true)
+		.withField('email', DataType.AD_HOC, true)
+
 		.withField('defaults.cash', DataType.BOOLEAN, true)
 		.withField('defaults.currency', DataType.forEnum(Currency, 'Currency'), true)
 		.withField('defaults.reinvest', DataType.BOOLEAN, true)
 		.withField('defaults.valuation', DataType.forEnum(ValuationType, 'ValuationType'), true)
-		.withField('miscellany', DataType.AD_HOC, true)
-		.withField('email', DataType.AD_HOC, true)
+
 		.schema
 	);
 
 	const update = new PortfolioSchema(SchemaBuilder.withName('update')
 		.withField('portfolio', DataType.STRING)
-		.withField('name', DataType.STRING)
+
 		.withField('timezone', DataType.forEnum(Timezones, 'Timezone'), true)
+		.withField('name', DataType.STRING)
+		.withField('miscellany', DataType.AD_HOC, true)
+		.withField('email', DataType.AD_HOC, true)
+
 		.withField('defaults.cash', DataType.BOOLEAN, true)
 		.withField('defaults.currency', DataType.forEnum(Currency, 'Currency'), true)
 		.withField('defaults.reinvest', DataType.BOOLEAN, true)
-		.withField('miscellany', DataType.AD_HOC, true)
-		.withField('email', DataType.AD_HOC, true)
+
 		.schema
 	);
 
